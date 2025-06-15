fb102ee4be0c9984689f915e25bc64c75a3c1a47
ulviasadov <ulviasadovv@gmail.com>
2025-06-15 21:09:00 +0400

    Create HomeIndexViewModel

--- Restoran_Project/Controllers/HomeController.cs
+++ Restoran_Project/Controllers/HomeController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Restoran_Project.Data;
 
 namespace Restoran_Project.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var chefs = _context.Chefs.ToList();
+            var customers = _context.Customers.ToList();
+
+            var viewModel = new ViewModels.HomeIndexViewModel
+            {
+                Chefs = chefs,
+                Customers = customers,
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ Restoran_Project/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Restoran_Project.Data;
+
+namespace Restoran_Project.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public List<Chef> Chefs { get; set; } = [];
+        public List<Customer> Customers { get; set; } = [];
+    }
+}
