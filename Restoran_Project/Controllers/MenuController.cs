22e83f1398b3396c5284f728de1e072b92ea7c2f
ulviasadov <ulviasadovv@gmail.com>
2025-06-16 02:24:12 +0400

    Add some menu pages

---
+++ Restoran_Project/Controllers/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restoran_Project.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Classic()
+        {
+            return View();
+        }
+
+        public IActionResult Board()
+        {
+            return View();
+        }
+
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+
+        public IActionResult Grid()
+        {
+            return View();
+        }
+
+        public IActionResult Simple()
+        {
+            return View();
+        }
+    }
+}
