e880c4750fbd6a537dd5cafd61740dc229411f7a
ulviasadov <ulviasadovv@gmail.com>
2025-06-16 13:56:50 +0400

    Update menus

---
+++ Restoran_Project/Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restoran_Project.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Carousel()
+        {
+            return View();
+        }
+
+        public IActionResult Grid()
+        {
+            return View();
+        }
+
+        public IActionResult Single()
+        {
+            return View();
+        }
+
+        public IActionResult Standard()
+        {
+            return View();
+        }
+    }
+}
---
+++ Restoran_Project/Controllers/ShopController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restoran_Project.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult FourCols()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult Single()
+        {
+            return View();
+        }
+    }
+}
